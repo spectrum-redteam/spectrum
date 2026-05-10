3047eae3d248ab2d4abbbc3a0ceb73395e3f8c9f
William Jiang <jiangwilliam30@gmail.com>
2026-05-10 14:17:55 +0800

    Delete redteamer directory

--- redteamer/modules/hash_util.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-from pathlib import Path
-
-BIN = None  # hashutil binary removed
-
-def hash_with_objc(input_str, algorithm="md5"):
-    raise NotImplementedError("[bold red]ObjC hashutil functionality removed.[/bold red]")
