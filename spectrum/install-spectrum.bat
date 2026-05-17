ac1fd320d258d3497d6311408d67742cc8c1891d
William Jiang <jiangwilliam30@gmail.com>
2026-05-17 13:58:54 +0800

    Delete spectrum directory

--- spectrum/install-spectrum.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing Spectrum Security...
-winget install -e --id Python.Python.3.11 --silent --accept-package-agreements
-call refreshenv
-pip install spectrum-security
-echo Spectrum installed. Run 'spectrum' to start.
--- spectrum/spectrum/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Spectrum - Autonomous Cyber Reasoning System"""
-__version__ = "1.35.0"
